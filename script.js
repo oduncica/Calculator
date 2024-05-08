7604b71f19120edb6e378174fa5560d636a2b224
oduncica <oduncica@gmail.com>
2024-05-08 21:08:10 +0200

    add subtract multiply and divide functions

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-let number1 = prompt("Enter number1");
-let number2 = prompt("Enter number2");
-
-
-addResult = parseInt(number1) + parseInt(number2);
 
 function addNumber(number1, number2){
-
-
+    addResult = parseInt(number1) + parseInt(number2);
     return addResult;
-
-    // return addResult;
-
 }
 
-const div = document.querySelector(".div");
+function subtractNumber(number1, number2){
+    subtractResult = parseInt(number1) - parseInt(number2);
+    return subtractResult;
+}
 
-div.textContent = addResult;
+function multiplyNumber(number1, number2){
+//     let number1 = prompt("Enter number1");
+// let number2 = prompt("Enter number2");
+    multiplyResult = parseInt(number1) * parseInt(number2);
+    const div = document.querySelector(".div");
+div.textContent = multiplyResult;
+
+}
+function divideNumber(number1, number2){
+    divideResult = parseInt(number1) / parseInt(number2);
+    return divideResult;
+}
+
+multiplyNumber();
+
+
 
 
