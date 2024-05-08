178b13ae2856bed2e9a699c8252621d614df1121
oduncica <oduncica@gmail.com>
2024-05-08 20:56:02 +0200

    addNumber function

---
+++ script.js
@@ -0,0 +1,20 @@
+let number1 = prompt("Enter number1");
+let number2 = prompt("Enter number2");
+
+
+addResult = parseInt(number1) + parseInt(number2);
+
+function addNumber(number1, number2){
+
+
+    return addResult;
+
+    // return addResult;
+
+}
+
+const div = document.querySelector(".div");
+
+div.textContent = addResult;
+
+
